a873f2d99a10d84b631c71501d823f3ffb16f3fc
Polina Grigorieva <polariyagv@mail.ru>
2022-06-26 17:03:14 +0500

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+
+if(B > max) max = B;
+if(C > max) max = C;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
