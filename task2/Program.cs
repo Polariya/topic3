bdd1a8acacbe6840089d3cd4941b42aa32b98254
Polina Grigorieva <polariyagv@mail.ru>
2022-06-26 16:59:22 +0500

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB){
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.Write(numberB);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+    Console.Write("min = ");
+    Console.Write(numberA);
+}
+ 
+
+
+
+
+
